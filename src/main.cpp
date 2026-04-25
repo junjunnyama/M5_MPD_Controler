6fd9698f0121a5a05866e516312530e719906931
junjun <2003junya@gmail.com>
2026-04-26 00:09:58 +0900

    表示方法をcanvasへ移行

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,12 @@
 // mDNS ライブラリ
 // #include <ESPmDNS.h>
 
+// M5Stack 画面用キャンバス
+M5Canvas canvas(&M5.Lcd);
+
 // WiFi setting
 const char ssid[] = "Your SSID";
-const char pass[] = "Your PASS";;
+const char pass[] = "Your PASS";
 
 // MPD setting
 MPD client;
@@ -36,54 +39,68 @@ uint16_t mpdport = 6600;
 MPDStatus mpdStatus;
 CurrentSongStatus currentSongStatus;
 
+// プロトタイプ宣言
+void ResetCanvas(int dir);
+
 void setup() {
   M5.begin();
-  Serial.begin(115200);
-  M5.Lcd.setRotation(1);
-  M5.Lcd.setCursor(0, 0);
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(1);
+  Serial.begin(115200);
+
+  // キャンバス初期化
+  canvas.createSprite(M5.Lcd.width(), M5.Lcd.height());
+  canvas.fillScreen(BLACK);
+  ResetCanvas(0);
 
   // WiFi connection
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    printEfont(".");
+    printEfont(&canvas, ".");
   }
-  // printEfont("\n");
-  printEfont("wifi connected\n");
+  printEfont(&canvas, "\n");
+  printEfont(&canvas, "wifi connected\n");
   
+  // MPD connection
   if (ip.fromString(mpdserver)){
     while (!client.connectMpd(ip, mpdport)) {
-      M5.Lcd.print(".");
+      canvas.print(".");
       delay(500);
     }
   }
-  // printEfont("\n");
-  printEfont("MPD connected\n");
+  printEfont(&canvas, "\n");
+  printEfont(&canvas, "MPD connected\n");
 
   Serial.println("MPD connected");
   mpdStatus = client.getStatus();
-  printEfont(String(mpdStatus.State) + "\n");
-  printEfont(String(mpdStatus.Volume) + "\n");
-  printEfont(String(mpdStatus.PlaylistLen) + "\n");
-  printEfont(String(mpdStatus.Random) + "\n");
-  printEfont(String(mpdStatus.Repeat) + "\n");
-  printEfont(String(mpdStatus.Single) + "\n");
-  printEfont(String(mpdStatus.PlaylistLen) + "\n");
-  printEfont(String(mpdStatus.PlayNum) + "\n");
-  printEfont(String(mpdStatus.Time) + "\n");
-  printEfont(String(mpdStatus.TotalTime) + "\n");
+  printEfont(&canvas, String(mpdStatus.State) + "\n");
+  printEfont(&canvas, String(mpdStatus.Volume) + "\n");
+  printEfont(&canvas, String(mpdStatus.PlaylistLen) + "\n");
+  printEfont(&canvas, String(mpdStatus.Random) + "\n");
+  printEfont(&canvas, String(mpdStatus.Repeat) + "\n");
+  printEfont(&canvas, String(mpdStatus.Single) + "\n");
+  printEfont(&canvas, String(mpdStatus.PlaylistLen) + "\n");
+  printEfont(&canvas, String(mpdStatus.PlayNum) + "\n");
+  printEfont(&canvas, String(mpdStatus.Time) + "\n");
+  printEfont(&canvas, String(mpdStatus.TotalTime) + "\n");
   
   Serial.println("Current MPD status");
   currentSongStatus = client.getCurrentSong();
-  printEfont(String(currentSongStatus.Title) + "\n");
-  printEfont(String(currentSongStatus.Artist) + "\n");
-  printEfont(String(currentSongStatus.Album) + "\n");
-  printEfont(String(currentSongStatus.Time) + "\n");
+  printEfont(&canvas, String(currentSongStatus.Title) + "\n");
+  printEfont(&canvas, String(currentSongStatus.Artist) + "\n");
+  printEfont(&canvas, String(currentSongStatus.Album) + "\n");
+  printEfont(&canvas, String(currentSongStatus.Time) + "\n");
 }
 
 void loop() {
   // printEfont(sprite, "Hello", 0, 16*0);
   // printEfont(sprite, "こんにちは", 0, 16*1);
+}
+
+void ResetCanvas(int dir){
+  canvas.clear();
+  canvas.setRotation(dir);
+  canvas.setCursor(0, 0);
+  printEfont(&canvas, "Direction: " + String(dir) + "\n");
 }
